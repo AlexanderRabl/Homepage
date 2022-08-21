8474f03ba16dd4b50be5f94c06604f14badfca34
Michael Legenstein <michael.legenstein@techtalk.at>
2022-08-21 23:09:17 +0200

    fix for legal notice

--- src/pages/legal-notice.js
+++ src/pages/legal-notice.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby'
 import Layout from '../components/layout/Layout';
-import { Trans, useTranslation } from 'gatsby-plugin-react-i18next';
+import { Trans, useTranslation, Link } from 'gatsby-plugin-react-i18next';
 import PageHeader from '../components/PageHeader';
 
 export const query = graphql`
@@ -26,15 +26,23 @@ const Contact = () => {
             <section id="legalNotice" className="py-10 lg:py-20">
                 <div className="container mx-auto px-8 tracking-wider">
                     <h1 className="text-lg lg:text-3xl font-semibold mb-8"><Trans>LegalNoticeHeader</Trans></h1>
-                    <p className="text-md lg:text-lg my-3 font-semibold" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticeAlex</Trans></p>
-                    <p className="text-md lg:text-lg  my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticeAddress</Trans></p>
-                    <p className="text-md lg:text-lg  my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticeContact</Trans></p>
-                    <p className="text-md lg:text-lg  my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticePlace</Trans></p>
-                    <p className="text-md lg:text-lg  my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticePurpose</Trans></p>
-                    <p className="text-md lg:text-lg  my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticeVAT</Trans></p>
-                    <p className="text-md lg:text-lg  my-3" style={{ whiteSpace: "pre-line" }}>
+                    <p className="text-md lg:text-lg my-3" style={{ whiteSpace: "pre-line" }}>
+                        <span className="font-semibold"><Trans>LegalNoticeAlex</Trans></span>
+                        <br/>
+                        <Trans>LegalNoticeAlexTitle</Trans>
+                    </p>
+                    <p className="text-md lg:text-lg my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticeAddress</Trans></p>
+                    <p className="text-md lg:text-lg my-3" style={{ whiteSpace: "pre-line" }}>
+                        <Trans>LegalNoticeContact</Trans>
+                        <br/>
+                        <Link to="/contact" className="text-primary-900 hover:underline"><Trans>LegalNoticeContactLink</Trans></Link>
+                    </p>
+                    <p className="text-md lg:text-lg my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticePlace</Trans></p>
+                    <p className="text-md lg:text-lg my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticePurpose</Trans></p>
+                    <p className="text-md lg:text-lg my-3" style={{ whiteSpace: "pre-line" }}><Trans>LegalNoticeVAT</Trans></p>
+                    <p className="text-md lg:text-lg my-3" style={{ whiteSpace: "pre-line" }}>
                         <Trans>LegalNoticeTax</Trans>
-                        <br></br>
+                        <br/>
                         <a className="text-primary-900 hover:underline" href={t('LegalNoticeTaxLink')} target="_blank"><Trans>LegalNoticeTaxLink</Trans></a>
                     </p>
                 </div>
