ee7e726d089dfed6a809e882daee752d1d0e8abc
Michael Legenstein <michael.legenstein@techtalk.at>
2022-08-22 20:09:20 +0200

    added 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,8 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout/Layout';
 import Button from '../components/Button';
 import HeroImage from '../svg/HeroImage';
+import LostImage from '../svg/LostImage';
+import SplitSection from '../components/SplitSection';
 
 export const query = graphql`
   query($language: String!) {
@@ -20,21 +22,21 @@ export const query = graphql`
 
 const NotFound = () => (
   <Layout>
-    <div className="md:w-1/3 mx-auto">
-      <HeroImage />
+    <div className="md:my-20">
+      <SplitSection
+        primarySlot={
+          <div className="">
+            <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+              <span className="sr-only">Error</span>404
+            </h2>
+            <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+            <p className="mt-4 mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+            <Button className="w-full px-4 py-2 mt-4 text-sm font-medium uppercase lg:w-auto" role="link">Back to homepage</Button>
+          </div>
+        }
+        secondarySlot={<LostImage />}
+      />
     </div>
-    <section className="flex items-center h-full p-16 text-primary-700">
-      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-        <div className="max-w-md text-center">
-          <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
-            <span className="sr-only">Error</span>404
-          </h2>
-          <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
-          <p className="mt-4 mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
-          <Button className="w-full px-4 py-2 mt-4 text-sm font-medium uppercase lg:w-auto" role="link">Back to homepage</Button>
-        </div>
-      </div>
-    </section>
   </Layout>
 );
 
